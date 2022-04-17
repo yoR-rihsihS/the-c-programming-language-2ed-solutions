16da3d01ec816e81c297dc2e62f09a79c4c0bb79
Shishir Roy <srshishirroy@gmail.com>
2022-04-17 17:11:49 +0530

    0.1

---
+++ Chapter 1/Exercise_1_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("1. Hello,\c World");
+    printf("2. Hello,\\ World");
+    printf("3. Hello,\t World");
+    printf("4. Hello,\n World");
+    printf("5. Hello,\() World");
+    return 0;
+}
